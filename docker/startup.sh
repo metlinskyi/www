c954819e2e5ad2d2623c404215eb5d657b7155f2
Roman Metlinskyi <gmail@metlinskyi.com>
2019-02-06 17:03:19 +0200

    startup

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,12 +2,12 @@
 
 if [ ! -d /usr/share/app/src ]; then
     ng new $projectName --directory .
-    find . -type d -exec chmod 700 {} \; && find . -type f -exec chmod 600 {} \;
+    find . -type d -exec chmod 755 {} \; && find . -type f -exec chmod 644 {} \;
 fi
 
 if [ ! -d /usr/share/app/src/node_module ]; then
     npm install --save @angular/material @angular/cdk @angular/animations
-    find . -type d -exec chmod 700 {} \; && find . -type f -exec chmod 600 {} \;
+    find . -type d -exec chmod 755 {} \; && find . -type f -exec chmod 644 {} \;
 fi
 
 ng serve --port 8081 >/dev/null &
