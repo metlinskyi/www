6f80b26041b45054471a7566f9ac362ddec4a031
Roman Metlinskyi <gmail@metlinskyi.com>
2019-02-06 22:31:33 +0200

    startup

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,8 +3,12 @@
 if [ ! -d /usr/share/app/src ]; then
     ng new $projectName --style=$style --prefix=$prefix --routing=true --skipInstall=true --verbose=true --force=true --directory . 
     sed -i "s/dist\\/$projectName/dist/g" angular.json
-    npm install && npm update
     chmod -R 777 .
+fi
+
+if [ ! -d /usr/share/app/node_modules ]; then
+    npm install && npm update
+    chmod -R 777 node_modules
 fi
 
 ng build && chmod -R 777 dist
