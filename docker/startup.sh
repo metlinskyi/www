67594c18774dbe5b5069da1ea9605bf89550116f
Roman Metlinskyi <gmail@metlinskyi.com>
2019-02-06 22:19:09 +0200

    refactoring

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,13 +2,11 @@
 
 if [ ! -d /usr/share/app/src ]; then
     ng new $projectName --style=$style --prefix=$prefix --routing=true --skipInstall=true --verbose=true --force=true --directory . 
+    sed -i "s/dist\\/$projectName/dist/g" angular.json
+    npm install && npm update
     chmod -R 777 .
 fi
 
-npm install 
-npm update
-chmod -R 777 node_modules
-
-ng build 
+ng build && chmod -R 777 dist
 ng serve >/dev/null &
 nginx -g 'daemon off;'
