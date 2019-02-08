90dffa01170d008b9b1b8e1ed8551664018e055b
Roman Metlinskyi <gmail@metlinskyi.com>
2019-02-08 12:00:08 +0200

    LF

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,17 +1,14 @@
-#!/usr/bin/env sh
-
+#!/bin/sh
 if [ ! -d /usr/share/app/src ]; then
     ng new $projectName --style=$style --prefix=$prefix --routing=true --skipInstall=true --verbose=true --force=true --directory . 
     sed -i "s/dist\\/$projectName/dist/g" angular.json
     chmod -R 777 .
 fi
-
 if [ ! -d /usr/share/app/node_modules ]; then
     npm install --save @angular/material @angular/cdk @angular/animations
     npm install && npm update
     chmod -R 777 node_modules
 fi
-
 ng build && chmod -R 777 dist
 ng serve >/dev/null &
 nginx -g 'daemon off;'
